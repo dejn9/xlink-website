75c6679082d8c13cbdd67b03b56668679dd74425
email <dejnns55@gmail.com>
2020-12-20 10:50:38 +0100

    questions

---
+++ js/questions.js
@@ -0,0 +1,63 @@
+let q1 = document.querySelector('.q1');
+let a1 = document.querySelector('.a1');
+
+let q2 = document.querySelector('.q2');
+let a2 = document.querySelector('.a2');
+
+let q3 = document.querySelector('.q3');
+let a3 = document.querySelector('.a3');
+
+let q4 = document.querySelector('.q4');
+let a4 = document.querySelector('.a4');
+
+let q5 = document.querySelector('.q5');
+let a5 = document.querySelector('.a5');
+
+let q6 = document.querySelector('.q6');
+let a6 = document.querySelector('.a6');
+
+let q7 = document.querySelector('.q7');
+let a7 = document.querySelector('.a7');
+
+let q8 = document.querySelector('.q8');
+let a8 = document.querySelector('.a8');
+
+let q9 = document.querySelector('.q9');
+let a9 = document.querySelector('.a9');
+
+q1.addEventListener('click',function(){
+    a1.classList.toggle('d-block');
+})
+
+q2.addEventListener('click',function(){
+    a2.classList.toggle('d-block');
+})
+
+q3.addEventListener('click',function(){
+    a3.classList.toggle('d-block');
+})
+
+q4.addEventListener('click',function(){
+    a4.classList.toggle('d-block');
+})
+
+q5.addEventListener('click',function(){
+    a5.classList.toggle('d-block');
+})
+
+q6.addEventListener('click',function(){
+    a6.classList.toggle('d-block');
+})
+
+q7.addEventListener('click',function(){
+    a7.classList.toggle('d-block');
+})
+
+q8.addEventListener('click',function(){
+    a8.classList.toggle('d-block');
+})
+
+q9.addEventListener('click',function(){
+    a9.classList.toggle('d-block');
+})
+
